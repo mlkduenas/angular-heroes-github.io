1e731d2b9a741b45722b083f22478085813eb5f9
Melky <melky@melky.com>
2021-11-16 11:17:37 -0300

    PWEB 16-11-2021 - Tour of Heroes - pts 3 e 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,20 +13,29 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
 
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[] = [];
   
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
-  constructor() { }
+  constructor(private heroService: HeroService,
+              private messageService: MessageService) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
 }
 
 }
